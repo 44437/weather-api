3b6bc769cc53d19e6e143f35710f903768b52abc
44437 <ercantopuz33@gmail.com>
2025-03-14 08:55:28 +0300

    Enable the api

--- cmd/weather-api/main.go
+++ cmd/weather-api/main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"log"
+	"os"
+	"weather-api/internal/config"
 	"weather-api/internal/server"
+	"weather-api/internal/weather"
 
 	"go.uber.org/zap"
 )
 
 func main() {
-	srv := server.NewServer()
-
 	logger, _ := zap.NewProduction()
 	zap.ReplaceGlobals(logger)
 
-	err := srv.Start()
+	appEnv := os.Getenv("APP_ENV")
+	conf, err := config.New(".config", appEnv)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	conf.Print()
+
+	handler := weather.NewHandler(weather.NewService(weather.NewRepository(conf.Postgres)))
+
+	handlers := []server.Handler{handler}
+
+	srv := server.NewServer(conf.Server.Port, handlers)
+
+	err = srv.Start()
 	if err != nil {
 		log.Fatalln(err)
 	}
