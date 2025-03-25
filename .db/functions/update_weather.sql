b68283fadd7167bf38a4f2f3d97ecc5edd982997
44437 <ercantopuz33@gmail.com>
2025-03-25 10:36:56 +0300

    Handle the additional request problem in which 10 requests arrive before time is over

--- .db/functions/update_weather.sql
+++ .db/functions/update_weather.sql
@@ -5,7 +5,8 @@ BEGIN
     SET 
         service_1_temperature = service_1_temp,
         service_2_temperature = service_2_temp,
-        request_count = 0
+        request_count = 0,
+        first_request_uuid = '00000000-0000-0000-0000-000000000000'
     WHERE id = location_id;
 END;
 $$ LANGUAGE plpgsql;
